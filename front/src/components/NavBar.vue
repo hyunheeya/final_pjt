c116bb7ac1234f604f3ebe5444c3ac167d754eb9
kmj <kmjk1117@gmail.com>
2024-11-18 15:25:33 +0900

    1-3순위_components_완료

---
+++ front/src/components/NavBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+    <nav>
+    <RouterLink :to="{name:'home'}">Home</RouterLink> | 
+    <RouterLink :to="{name:'productsRecommend'}">예적금 상품 추천</RouterLink> | 
+    <RouterLink :to="{name:'productslist'}">예적금 상품 전체 리스트</RouterLink> | 
+    <RouterLink :to="{name:'signup'}">회원가입</RouterLink> | 
+    <RouterLink :to="{name:'login'}">로그인</RouterLink> | 
+    <RouterLink :to="{name:'profile'}">마이페이지</RouterLink>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ front/src/views/ProductsRecommendView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>예적금 상품 추천 </h1>
+    <RouterLink :to="{name:'productsrecommendquiz'}">
+      <button>시작하기</button>
+    </RouterLink>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
