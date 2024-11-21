ad9217c675487fb3ecabf8d5d4f8a5a1ca42874c
nansae0804 <nansae0804@gmail.com>
2024-11-21 13:59:14 +0900

    추천 상품 결과 출력 필드 수정

--- front/src/views/ProductsRecommendResultView.vue
+++ front/src/views/ProductsRecommendResultView.vue
@@ -5,9 +5,12 @@
       <li v-for="result in results" :key="result.name">
         <p><strong>금융회사:</strong> {{ result.name }}</p>
         <p><strong>금리:</strong> {{ result.interest_rate }}%</p>
-        <p><strong>납입 기간:</strong> {{ result.save_trm }}</p>
+        <p><strong>납입 기간:</strong> {{ result.save_trm }}개월</p>
+        
+        <!-- 상세 조회가 끝나고 나서 다시 시작 -->
+        <!-- <p><strong>product_type:</strong> {{ result.product_type }}</p> -->
         <!-- <div>
-          <router-link :to="{ name: 'productsdepositlistdetail' }">
+          <router-link :to="{ name: 'productsdepositlistdetail', params: result.fin_prdt_nm }">
             <button>Detail</button>
           </router-link>
         </div> -->
