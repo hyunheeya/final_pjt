c7d7728b9d97affd913301c8d61e777d9124d90d
kmj <kmjk1117@gmail.com>
2024-11-24 02:49:55 +0900

    예적금추천페이지 부트스트랩적용

--- front/src/views/ProductsRecommendView.vue
+++ front/src/views/ProductsRecommendView.vue
@@ -1,8 +1,8 @@
 <template>
-  <div>
-    <h1>예적금 상품 추천 </h1>
-    <RouterLink :to="{name:'productsrecommendquiz'}">
-      <button>시작하기</button>
+  <div class="container text-center mt-5">
+    <h1 class="display-4 mb-4">예적금 상품 추천</h1>
+    <RouterLink :to="{name: 'productsrecommendquiz'}">
+      <button class="btn btn-primary btn-lg">시작하기</button>
     </RouterLink>
   </div>
 </template>
