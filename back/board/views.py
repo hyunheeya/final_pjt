1fb401f9d02ed4840a2f4effe27951f831a607f0
gusgml9718 <gusgml9718@knu.ac.kr>
2024-11-23 16:43:58 +0900

    board 미완

--- back/board/views.py
+++ back/board/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from django.http import JsonResponse
 from rest_framework.response import Response # DRF
 from rest_framework import status
@@ -12,26 +12,36 @@ from .models import Article
 
 @api_view(['GET', 'POST'])
 @permission_classes([IsAuthenticated])
+# @permission_classes([IsAdminUser])
 def article_list(request):
     if request.method == 'GET':
         articles = get_list_or_404(Article)
         serializer = ArticleListSerializer(articles, many=True)
         return Response(serializer.data)
-    
+
     elif request.method == 'POST':
         serializer = ArticleSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             serializer.save()
-            # serializer.save(user=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
     
 
-@api_view(['POST'])
+@api_view(['GET', 'PUT', 'DELETE'])
 @permission_classes([IsAuthenticated])
+# @permission_classes([IsAdminUser])
 def article_detail(request, article_pk):
     article = get_object_or_404(Article, pk=article_pk)
 
     if request.method == 'GET':
         serializer = ArticleSerializer(article)
-        print(serializer.data)
         return Response(serializer.data)
+    
+    elif request.method == 'PUT':
+        serializer = ArticleSerializer(article, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+
+    elif request.method == 'DELETE':
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
