c4ea0b449b2cd4ab8e6e961202bc70db01e5d842
gusgml9718 <gusgml9718@knu.ac.kr>
2024-11-26 02:25:25 +0900

    noto sans 폰트 적용 완료

--- front/src/main.js
+++ front/src/main.js
@@ -3,13 +3,15 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import '@/assets/main.css'
 
 // npm install bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import 'bootstrap-icons/font/bootstrap-icons.css'
+
+import './assets/main.css'
+
 const app = createApp(App)
 
 app.use(createPinia())
